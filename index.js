5d98698126e2fbfc70201ec674121d7f3ccba924
KhanhCoder256 <khanhbuinguyen256@gmail.com>
2022-01-30 15:51:28 +0700

    update 2

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+let canvas, width, height, ctx;
+let fireworks =[]
+let particles = [];
+
+function setup() {
+    canvas = document.getElementById('canvas');
+    setSize(canvas);
+    ctx = canvas.getContext("2d");
+    ctx.fillstyle = "#000000";
+    ctx.fillRect(0, 0, width, height);
+    fireworks.push(new fireworks(Math.random()*(width-200)+100));
+    window.addEventListener("resize",windowResized);
+    document.addEventListener("click",onclick);
+}
+
+setTimeout(setup,1);
+
+function loop(){
+    ctx.globalAlpha = 0.1;
+    ctx.fillstyle = "#000000";
+    ctx.fillRect(0, 0, width, height);
+    ctx.globalAlpha = 1;
+    
+    for(let i=0; i<fireworks.length; i++){
+        let done = fireworks[i].update();
+        fireworks[i].draw();
+        if(done) fireworks.splice(i ,1);
+    }
+    for (let i=0; i<particles.length; i++)
+    particles[i].update();
+    particles[i].draw();
+    if(particles[i].lifetime>80) particles.splice(i ,1);
+
+}
